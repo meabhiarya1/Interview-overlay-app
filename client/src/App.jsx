a2c71370af32e7589fa5456e957f1286ce2a7757
Abhishek kumar <abhi.arya97161@gmail.com>
2025-04-14 14:24:39 +0530

    Remove unused back navigation and new tab functionality from App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,18 +4,6 @@ import "./App.css";
 function App() {
   const [url, setUrl] = useState("https://www.google.com");
 
-  const handleBack = () => {
-    const view = document.querySelector("webview");
-    if (view && view.canGoBack()) {
-      view.goBack();
-    }
-  };
-
-  const handleNewTab = () => {
-    const newWindow = window.open(url, "_blank", "width=800,height=600");
-    if (newWindow) newWindow.focus();
-  };
-
   useEffect(() => {
     if (window.electronAPI) {
       window.electronAPI.onGoBack(() => {
@@ -29,15 +17,6 @@ function App() {
 
   return (
     <div className="app">
-      {/* Make the control bar draggable */}
-      {/* <div className="controls">
-        <button onClick={handleBack} style={{ WebkitAppRegion: "no-drag" }}>
-          Back
-        </button>
-        <button onClick={handleNewTab} style={{ WebkitAppRegion: "no-drag" }}>
-          New Tab
-        </button>
-      </div> */}
       <webview
         src="https://www.google.com"
         style={{ width: "100vw", height: "100vh", border: "none" }}
